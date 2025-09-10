21ceac6982594e69a0a344e4391fe99502bc7afc
Tien Nguyen <tien.nguyen@threatfabric.com>
2025-09-10 22:11:01 +0200

    feat: add tracing for logging http request

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use rus3::{router::setup_router, service::s3client::S3Client, state::AppState};
 use std::net::SocketAddr;
 use std::sync::Arc;
+use tracing::{error, info};
+use tracing_subscriber::{EnvFilter, fmt};
 
 #[tokio::main]
 async fn main() {
+    fmt().with_env_filter(EnvFilter::from_default_env()).init();
+
+    info!("Starting rus3 server...");
+
     let client = S3Client::new();
 
     let state = AppState {
@@ -12,11 +18,13 @@ async fn main() {
 
     let app = setup_router(state);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     println!("listening on {}", addr);
 
-    axum_server::bind(addr)
+    if let Err(err) = axum_server::bind(addr)
         .serve(app.await.into_make_service())
         .await
-        .unwrap();
+    {
+        error!("Server error: {:?}", err);
+    }
 }
--- src/router.rs
+++ src/router.rs
@@ -3,6 +3,7 @@ use crate::{
     state::AppState,
 };
 use axum::{Router, routing::get};
+use tower_http::trace::TraceLayer;
 
 pub async fn setup_router(state: AppState) -> Router {
     Router::new()
@@ -10,4 +11,25 @@ pub async fn setup_router(state: AppState) -> Router {
         .route("/bucket/{bucket}", get(object::list_objects))
         .route("/bucket/{bucket}/{*prefix}", get(object::list_objects))
         .with_state(state)
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|request: &axum::http::Request<_>| {
+                    tracing::info_span!(
+                        "http-request",
+                        method = %request.method(),
+                        uri = %request.uri()
+                    )
+                })
+                .on_response(
+                    |response: &axum::http::Response<_>,
+                     latency: std::time::Duration,
+                     _span: &tracing::Span| {
+                        tracing::info!(
+                            status = %response.status(),
+                            latency_ms = %latency.as_millis(),
+                            "response finished"
+                        );
+                    },
+                ),
+        )
 }
